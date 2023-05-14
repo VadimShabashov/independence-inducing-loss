Fix independence calculation, decrease independence loss constant, remove L2, add experiment

losses/independence.py
@@ -1,6 +1,7 @@
 import torch
 
 from whitening import ZCA1, ZCA2, NoWhitening
+from utils import non_diagonal_correlation
 
 
 class BaseIndependenceLoss:
@@ -34,7 +35,7 @@ class NegApproxLoss1(BaseIndependenceLoss):
     Minimization of mutual information <=> maximization of negentropy <=> minimization of 1/negentropy
     """
 
-    def __init__(self, device, whitening, embedding_dim, alpha=1.0, eps=1e-8):
+    def __init__(self, device, whitening, embedding_dim, alpha=5.0, eps=1e-8):
         super().__init__(device, whitening, embedding_dim, alpha, eps)
 
     def __call__(self, x):
@@ -110,16 +111,4 @@ class CorrMatLoss(BaseIndependenceLoss):
         super().__init__(device, whitening, embedding_dim, alpha, eps)
 
     def __call__(self, x):
-        # Apply whitening
-        x = self.apply_whitening(x)
-
-        # Calculate correlation matrix
-        correlation_matrix = torch.corrcoef(x.T)
-
-        # Create mask for diagonal elements
-        diagonal_elements_mask = torch.eye(correlation_matrix.shape[0], dtype=torch.bool)
-
-        # Get non-diagonal elements
-        non_diagonal_elements = correlation_matrix[~diagonal_elements_mask]
-
-        return self.alpha * non_diagonal_elements.abs().mean()
+        return self.alpha * non_diagonal_correlation(x)

metrics_storage.py
@@ -2,6 +2,8 @@ import re
 import torch
 from torchmetrics import RetrievalMAP
 
+from utils import non_diagonal_correlation
+
 
 class MetricsStorage:
     def __init__(self, metrics, database_embeddings, database_labels, query_embeddings, query_labels):
@@ -17,8 +19,8 @@ class MetricsStorage:
                     (self.calculate_sparsity(database_embeddings), self.calculate_sparsity(query_embeddings))
                 )
             elif metric == 'Independence':
-                self.metrics['Independence'] = torch.cat(
-                    (self.calculate_independence(database_embeddings), self.calculate_independence(query_embeddings))
+                self.metrics['Independence'] = 0.5 * (
+                    self.calculate_independence(database_embeddings) + self.calculate_independence(query_embeddings)
                 )
             elif p_at_k_pattern.match(metric) or metric == 'MAP':
                 ranking_metrics.append(metric)
@@ -36,17 +38,7 @@ class MetricsStorage:
 
     @staticmethod
     def calculate_independence(embeddings):
-        # Calculate correlation matrix
-        correlation_matrix = torch.corrcoef(embeddings.T)
-
-        # Replace nan with 0 (nan appears due to constant value -> zero variance)
-        correlation_matrix[torch.isnan(correlation_matrix)] = 0.0
-
-        # Find non-diagonal elements of correlation matrix
-        non_diagonal_correlation_matrix = torch.abs(correlation_matrix) - torch.eye(correlation_matrix.shape[0])
-
-        # For each feature find mean over its correlation with others
-        return non_diagonal_correlation_matrix.mean(dim=1)
+        return non_diagonal_correlation(embeddings)
 
     def calculate_ranking_metrics(self, database_embeddings, database_labels, query_embeddings, query_labels, ranking_metrics):
         # For each query embedding calculate distances to embeddings in database