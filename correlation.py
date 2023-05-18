2a297a5df783628376c0640495a4367cefe96db0
Vadim Shabashov <vadim.art.shabashov@gmail.com>
2023-05-19 00:55:53 +0300

    Correct correlation matrix calculation

--- correlation.py
+++ correlation.py
@@ -9,6 +9,9 @@ def non_diagonal_correlation(x):
     # Calculate correlation matrix
     correlation_matrix = torch.corrcoef(x.T)
 
+    # Replace nan with 0 (nan appears due to constant value -> zero variance)
+    correlation_matrix[torch.isnan(correlation_matrix)] = 0.0
+
     # Create mask for diagonal elements
     diagonal_elements_mask = torch.eye(correlation_matrix.shape[0], dtype=torch.bool)
 
